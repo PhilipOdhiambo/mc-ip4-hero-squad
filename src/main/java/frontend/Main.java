71b7f26456f39386ebecb2e6368dcb3b7e654e71
Philip Odhiambo <jofonowino@gmail.com>
2021-08-08 20:42:02 +0300

    add Sql2o and H2

---
+++ src/main/java/frontend/Main.java
@@ -0,0 +1,19 @@
+package frontend;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.*;
+
+public class Main {
+    public static void main(String[] args) {
+        get("/",(req,res) -> {
+            Map<String,Object> modal = new HashMap<>();
+            return new ModelAndView(modal, "new-squad.hbs");
+        }, new HandlebarsTemplateEngine());
+
+
+    }
+}
