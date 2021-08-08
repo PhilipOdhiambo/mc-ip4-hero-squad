20e73a8947a2e65e7c79ae764b9182a624bba0ca
Philip Odhiambo <jofonowino@gmail.com>
2021-08-08 22:39:11 +0300

    Squad class created. Corresponding tests pass

--- src/main/java/models/Squad.java
+++ src/main/java/models/Squad.java
@@ -1,4 +1,54 @@
 package models;
 
 public class Squad {
+    int id;
+    int maxSize;
+    String name;
+    String cause;
+
+    // Constructor function
+    public Squad(
+            int maxSize,
+            String name,
+            String cause
+    ) {
+        this.name = name;
+        this.maxSize = maxSize;
+        this.cause = cause;
+    }
+
+    // Getters and setters
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getMaxSize() {
+        return maxSize;
+    }
+
+    public void setMaxSize(int maxSize) {
+        this.maxSize = maxSize;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCause() {
+        return cause;
+    }
+
+    public void setCause(String cause) {
+        this.cause = cause;
+    }
 }
--- src/test/java/models/SquadTest.java
+++ src/test/java/models/SquadTest.java
@@ -1,7 +1,39 @@
 package models;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SquadTest {
+
+    @Test
+    void newSquadCreatedCorrectly_true() {
+        Squad squad = setupNewSquad();
+        assertTrue(squad instanceof Squad);
+    }
+
+    @Test
+    void newSquad_nameInstantiatedCorrectly_string() {
+        Squad squad = setupNewSquad();
+        assertEquals("squad1", squad.getName());
+    }
+
+    @Test
+    void newSquad_maxNumberInitiateCorrectly_int() {
+        Squad squad = setupNewSquad();
+        assertEquals(1,squad.getMaxSize());
+    }
+
+    @Test
+    void newSquad_causeInitiatedCorrectly_string() {
+        Squad squad = setupNewSquad();
+        assertEquals("sexism", squad.getCause());
+    }
+
+
+    // helper methods
+    public  Squad setupNewSquad() {
+        return new Squad(1,"squad1","sexism");
+    }
 
 }
