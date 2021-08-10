bd6e00027bd7a6feda3591d4c3ab04fbd3a445ff
Philip Odhiambo <jofonowino@gmail.com>
2021-08-10 10:48:03 +0300

    chage connection parameters

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,7 @@
+import dao.Sql2oHeroDao;
+import dao.Sql2oSquadDao;
+import models.Squad;
+import org.sql2o.Sql2o;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
@@ -7,12 +11,37 @@ import java.util.Map;
 import static spark.Spark.*;
 
 public class App {
+
     public static void main(String[] args) {
+        staticFileLocation("/public");
+        String connectionString = "jdbc:postgresql://localhost:5432/heroSquad";
+        Sql2o sql2o = new Sql2o(connectionString, "philip", "1234");
+        Sql2oHeroDao heroDao = new Sql2oHeroDao(sql2o);
+        Sql2oSquadDao squadDao = new Sql2oSquadDao(sql2o);
+
+
+        // Landing page
         get("/",(req,res) -> {
             Map<String,Object> modal = new HashMap<>();
-            return new ModelAndView(modal, "new-squad.hbs");
+            return new ModelAndView(modal, "landing-page.hbs");
         }, new HandlebarsTemplateEngine());
 
+        // Hero-squad page
+        get("/hero-squad", (req,res) -> {
+            Map<String,Object> model = new HashMap<>();
+            return new ModelAndView(model,"hero-squad.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        // Add new squad
+        post("/new-hero-squad",(req,res) -> {
+            String squadName = req.queryParams("new-squad-name");
+            String squadCause = req.queryParams("new-squad-cause");
+            String squadMaxSize = req.queryParams("new-squad-maxSize");
+
+            Squad squad = new Squad(Integer.parseInt(squadMaxSize),squadName,squadCause);
+
+            return null;
+        });
 
     }
 }
